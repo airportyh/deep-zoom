784b540bd61da5e0da9adeb0322a102811edc3ac
Toby Ho <airportyh@gmail.com>
2020-03-23 15:37:19 -0400

    Got zooming and panning to workgit status

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import * as FontMetrics from 'fontmetrics';
 const API_BASE_URL = "http://localhost:3000/fs";
+const CANVAS_WIDTH = 1000;
+const CANVAS_HEIGHT = 1000;
 
 type BoundingBox = {
     top: number;
@@ -67,12 +68,6 @@ function drawTextLines(
     box: BoundingBox) {
     ctx.fillStyle = "black";
     
-    const fontMetric = FontMetrics({
-        fontFamily,
-        fontWeight,
-        fontSize,
-        origin: "baseline"
-    });
     const topOffset = (box.height - lines.length * fontSize * lineHeight) / 2;
     const longestLineWidth = lines.reduce((longestWidth, line) => {
         const width = ctx.measureText(line.join(" ")).width;
@@ -84,7 +79,6 @@ function drawTextLines(
     }, 0);
     const leftOffset = (box.width - longestLineWidth) / 2;
     const maxSpaceWidth = ctx.measureText("O").width * 2;
-    const descent = fontSize * fontMetric.descent;
     lines.forEach((line, lineIdx) => {
         const onlyTextWidth = ctx.measureText(line.join("")).width;
         const availableWhiteSpace = box.width - onlyTextWidth;
@@ -94,7 +88,8 @@ function drawTextLines(
         line.forEach((word, wordIdx) => {
             ctx.fillText(word, 
                 leftOffset + box.left + runningLineWidth, 
-                topOffset + box.top + (lineIdx + 1) * (lineHeight * fontSize) - descent
+                topOffset + box.top + (lineIdx + 1) * 
+                    (lineHeight * fontSize)
             );
             runningLineWidth += ctx.measureText(word).width + 
                 spaceWidth;
@@ -155,63 +150,146 @@ function calculateTextLayout(
 
 
 async function main() {
-    const request = await fetch(API_BASE_URL + "?path=/");
+    let viewport = {
+        top: 0,
+        left: 0,
+        zoom: 1
+    };
+    let dragging = false;
+    let dragStartX: number;
+    let dragStartY: number;
+    const request = await fetch(API_BASE_URL + "?path=.git");
     const rootInfo = await request.json();
 
     const canvas = document.createElement("canvas");
-    canvas.width = 1000;
-    canvas.height = 1000;
-    canvas.style.transform = "scale(0.5) translate(-500px, -500px)";
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+    canvas.style.transform = `scale(0.5) translate(-${CANVAS_WIDTH / 2}px, -${CANVAS_HEIGHT / 2}px)`;
     canvas.style.border = "1px solid black";
 
     document.body.appendChild(canvas);
 
     const ctx = canvas.getContext("2d");
-    // ctx.textBaseline = "top";
+    ctx.textBaseline = "bottom";
 
-    // console.log(rootInfo);
-    fitText(ctx, "./", "Monaco", "normal", {
-        top: 0, left: 0, width: 1000, height: 1000
+    render();
+
+    window.addEventListener("mousedown", (e: MouseEvent) => {
+        dragging = true;
+        [dragStartX, dragStartY] = pointScreenToCanvas(e);
     });
-    const entries = rootInfo.entries;
-    const perRow = 3;
-    const columnWidth = 1000 / perRow;
-    const rowHeight = 1000 / perRow;
-    for (let i = 0; i < entries.length; i++) {
-        const entry = entries[i];
-        const rowIndex = Math.floor(i / perRow);
-        const columnIndex = i % perRow;
-        fitText(ctx, entry, "Monaco", "normal", {
-            top: rowIndex * rowHeight,
-            left: columnIndex * columnWidth,
-            width: columnWidth,
-            height: rowHeight
-        });
+
+    window.addEventListener("mouseup", () => {
+        dragging = false;
+    });
+
+    window.addEventListener("mousemove", (e: MouseEvent) => {
+        if (dragging) {
+            const [pointerX, pointerY] = pointScreenToCanvas(e);
+            const [worldPointerX, worldPointerY] = pointCanvasToWorld(pointerX, pointerY);
+            const [worldDragStartX, worldDragStartY] = pointCanvasToWorld(dragStartX, dragStartY);
+            viewport.left -= worldPointerX - worldDragStartX;
+            viewport.top -= worldPointerY - worldDragStartY;
+            dragStartX = pointerX;
+            dragStartY = pointerY;
+            requestAnimationFrame(render);
+        }
+    });
+
+    window.addEventListener("wheel", function (e: any) {
+        e.preventDefault();
+        const delta = e.deltaY;
+        const [pointerX, pointerY] = pointScreenToCanvas(e);
+        const newZoom = Math.max(0.5, viewport.zoom - delta * 0.01);
+
+        const [worldPointerX, worldPointerY] = pointCanvasToWorld(pointerX, pointerY);
+        const newLeft = - (pointerX / newZoom - worldPointerX);
+        const newTop = - (pointerY / newZoom - worldPointerY);
+        const newViewport = {
+            top: newTop,
+            left: newLeft,
+            zoom: newZoom
+        };
+        viewport = newViewport;
+        
+        // console.log("x=", pointerX, "y=", pointerY, "zoomLevel=", zoomLevel);
+        
+        this.requestAnimationFrame(render);
+      }, { passive: false });
+
+    function pointScreenToCanvas(e: MouseEvent): [number, number] {
+        return [
+            (e.clientX - canvas.offsetLeft - 1) * 2,
+            (e.clientY - canvas.offsetTop - 1) * 2
+        ];
+    }
+    
+    function pointCanvasToWorld(x: number, y: number): [number, number] {
+        return [
+            x / viewport.zoom + viewport.left,
+            y / viewport.zoom + viewport.top
+        ];
     }
 
-    // document.addEventListener("wheel", (evt) => {
-    //     evt.preventDefault();
-    //     console.log("wheel", evt);
-    // });
-    // canvas.addEventListener("pointerup", (evt) => {
-    //     console.log("pointerup", evt);
-    // });
+    function pointWorldToCanvas(x: number, y: number): [number, number] {
+        return [
+            (x - viewport.top) * viewport.zoom,
+            (y - viewport.left) * viewport.zoom
+        ];
+    }
 
-    window.addEventListener("wheel", function (e) {
-        e.preventDefault();
-        console.log("wheel", e);
-      
-        // if (e.ctrlKey) {
-        //   // Your zoom/scale factor
-        //   scale -= e.deltaY * 0.01;
-        // } else {
-        //   // Your trackpad X and Y positions
-        //   posX -= e.deltaX * 2;
-        //   posY -= e.deltaY * 2;
+    function boxCanvasToWorld(box: BoundingBox): BoundingBox {
+        return {
+            top: box.top / viewport.zoom + viewport.top,
+            left: box.left / viewport.zoom + viewport.left,
+            width: box.width / viewport.zoom,
+            height: box.height / viewport.zoom
+        };
+    }
+
+    function boxWorldToCanvas(box: BoundingBox): BoundingBox {
+        return {
+            top: (box.top - viewport.top) * viewport.zoom,
+            left: (box.left - viewport.left) * viewport.zoom,
+            width: box.width * viewport.zoom,
+            height: box.height * viewport.zoom
+        };
+    }
+
+    function render() {
+        ctx.save();
+        ctx.fillStyle = "while";
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        ctx.restore();
+        fitText(ctx, ".git", "Monaco", "normal", boxWorldToCanvas({
+            top: 0, left: 0, width: CANVAS_WIDTH, height: CANVAS_HEIGHT
+        }));
+        const entries = rootInfo.entries;
+        const perRow = Math.ceil(Math.sqrt(entries.length));
+        const columnWidth = CANVAS_WIDTH / perRow;
+        const rowHeight = CANVAS_HEIGHT / perRow;
+        for (let i = 0; i < entries.length; i++) {
+            const entry = entries[i];
+            const rowIndex = Math.floor(i / perRow);
+            const columnIndex = i % perRow;
+            fitText(ctx, entry, "Monaco", "normal", boxWorldToCanvas({
+                top: rowIndex * rowHeight,
+                left: columnIndex * columnWidth,
+                width: columnWidth,
+                height: rowHeight
+            }));
+        }
+        // if (pointerX && pointerY) {
+        //     ctx.save();
+        //     ctx.fillStyle = "orange";
+        //     ctx.fillRect(
+        //         pointerX - 4, 
+        //         pointerY - 4, 
+        //         9, 
+        //         9);
+        //     ctx.restore();
         // }
-      
-        // render();
-      }, { passive: false });
+    }
 }
 
 main().catch((err) => console.log(err.stack));
